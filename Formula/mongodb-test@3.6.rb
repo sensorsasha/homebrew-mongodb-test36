08979c169de22e3195dbd1b8f87080b564c4c401
Sasha Elhovenko <sasha@sensortower.com>
2019-06-29 20:11:37 +0300

    specify version

--- Formula/mongodb-test@3.6.rb
+++ Formula/mongodb-test@3.6.rb
@@ -4,7 +4,7 @@ class MongodbTestAT36 < Formula
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   revision 1
-  version ""
+  version "3.6"
 
   # keg_only :versioned_formula
 
